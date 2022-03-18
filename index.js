8aef2f92520c37da65c77d898e23fc0c70b288b8
Iuri Dias <iuridias.mw@gmail.com>
2022-03-17 22:35:49 -0300

    Projeto final adicionado

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const customExpress = require('./config/custom-express');
 const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas');
 
 conexao.connect(erro => {
   if(erro) {
     console.log(erro)
   } else {
     console.log('Conexão com MySql bem sucedida!!');
+
+    Tabelas.init(conexao);
+
     const app = customExpress();
 
     app.listen(3000, () => console.log("Servidor rodando na porta 3000."));
